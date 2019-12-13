7efd3be19b5fd407a89b0f149b59f32dc79854d4
Rick Tillery <github@tilleryweb.com>
2019-12-12 22:59:51 -0600

    Checking for data from POST command

--- trnybrkt.js
+++ trnybrkt.js
@@ -289,26 +289,16 @@ var server = http.createServer(
 console.log(`req.url: ${req.url}\n`);
     if (req.method == 'POST') {
 console.log("POST ed");
-console.log("one");
-console.log("two");
-console.log("three");
-console.log("four");
-console.log("five");
-console.log("six");
-console.log("seven");
-console.log("eight");
-console.log("nine");
-console.log("ten");
       var body = '';
       req.on('data', function (data) {
+console.log(`RECEIVE: \"${data}\"`);
         body += data;
       });
       req.on('end', function (data) {
         teams = JSON.parse(body).teams;
-
-for (var i = 0; i < teams.length; i++) {
-  console.log(`${teams[i].friendlyName}`);
-}
+// for (var i = 0; i < teams.length; i++) {
+//   console.log(`${teams[i].friendlyName}`);
+// }
 
         options.groupName = JSON.parse(body).name;
 
